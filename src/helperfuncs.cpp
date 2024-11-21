562f3003823fa0dda48ebeaa496945d8a9b7d766
Gerhardus Coenraad Potgieter <25999656@sun.ac.za>
2024-11-21 12:00:55 +0200

    Matrix mult works, need to start thinking of error handling or might think of this at the end

--- src/helperfuncs.cpp
+++ src/helperfuncs.cpp
@@ -40,6 +40,7 @@ std::vector<std::vector<double>> matrixMultiply(const std::vector<std::vector<do
     // Now perform matrix mult
     const size_t numOutpRows = mat1.size();
     const size_t numOutpCols = mat2[0].size();
+    const size_t numInner = mat2.size();
 
     std::vector<std::vector<double>> outpMat(numOutpRows, std::vector<double>(numOutpCols, 0.0));
 
@@ -50,7 +51,7 @@ std::vector<std::vector<double>> matrixMultiply(const std::vector<std::vector<do
 
             // Calc element
             double runningSum = 0;
-            for (size_t inner = 0; inner < numOutpCols; inner++) {
+            for (size_t inner = 0; inner < numInner; inner++) {
                 runningSum += mat1[row][inner] * mat2[inner][col];
             }
             // Populate element
--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,19 @@ int main() {
 
 
     /* std::vector<std::vector<double>> myInput = { {1,2,3}, {4,5,6}, {7,8,9}, {10,11,12} }; */
-    std::vector<std::vector<double>> myInput = { {1,2,3}, {4,5,6}, {7,8,9} };
+    /* std::vector<std::vector<double>> myInput = { {1,2,3}, {4,5,6}, {7,8,9} }; */
+    std::vector<std::vector<double>> myInput = { {1,2,3,5}, {4,5,6,7}, {7,8,9,10} };
     std::vector<std::vector<double>> myInput2 = { {1,2,3,5}, {4,5,6,7}, {7,8,9,10} };
 
+    // this is alot of different words sfa fa da daofnaoF aof ojADN ajo fjoaD Ojanfojas vojasnd
     try{
         std::vector<std::vector<double>> outp = matrixMultiply(myInput, myInput2);
         printMatrix(outp);
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
+
+    std::cout << "Do things " << std::endl;
 
     return 0;
     std::vector<int> myStruct = { 2, 10, 5};
